19ff950710b65a8e64a2bb398cf6ee83ab9e6d82
azikreed <azikreed@gmail.com>
2022-06-11 11:54:11 +0500

    ready to push

--- src/main.js
+++ src/main.js
@@ -1,6 +1,15 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import { createApp } from "vue";
+import App from "./App";
+import components from "@/components/UI";
+import router from "./router/router";
+import VIntersection from "./directives/VIntersection";
 
-createApp(App).use(store).use(router).mount('#app')
+const app = createApp(App);
+
+components.forEach((component) => {
+  app.component(component.name, component);
+});
+
+app.directive("intersection", VIntersection);
+
+app.use(router).mount("#app");
---
+++ src/router/router.js
@@ -0,0 +1,31 @@
+import Main from "@/pages/Main.vue";
+import PostPage from "@/pages/PostPage.vue";
+import PostIdPage from "@/pages/PostIdPage.vue";
+import { createRouter, createWebHistory } from "vue-router";
+import About from "@/pages/About.vue";
+
+const routes = [
+  {
+    path: "/",
+    component: Main,
+  },
+  {
+    path: "/posts",
+    component: PostPage,
+  },
+  {
+    path: "/about",
+    component: About,
+  },
+  {
+    path: "/posts/:id",
+    component: PostIdPage,
+  },
+];
+
+const router = createRouter({
+  routes,
+  history: createWebHistory(process.env.BASE_URL),
+});
+
+export default router;
